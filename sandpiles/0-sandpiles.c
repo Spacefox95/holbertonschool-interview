5529c3ff597973d3aed42f4ce36799a6117b0123
Spacefox95 <nathan.raynal@gmail.com>
2024-09-25 09:42:41 +0200

    ok

---
+++ sandpiles/0-sandpiles.c
@@ -0,0 +1,15 @@
+#include "sandpiles.h"
+#include <stdio.h>
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+	int x, y;
+	for (x = 0; x < 3; x++)
+	{
+		for (y = 0; y < 3; y++)
+		{
+			grid1[x][y] += grid2[x][y];
+		}
+		
+	}
+}
