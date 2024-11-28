1e2d7c48e0f955887cc839cba4a980e23e5f1b41
Spacefox95 <nathan.raynal@gmail.com>
2024-11-28 10:22:03 +0100

    ok

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -12,16 +12,17 @@ def island_perimeter(grid: list[list[int]]) -> int:
     for row in range(rows):
         for col in range(cols):
             if grid[row][col] == 1:
-                if grid[row - 1][col] == 0 and row > 0:
+                print(grid[row][col])
+                if row > 0 and grid[row - 1][col] == 0:
                     perimeter += 1
                 if row == 0:
                     perimeter += 1
-                if grid[row + 1][col] == 0 and row < rows - 1:
+                if row < rows - 1 and grid[row + 1][col] == 0:
                     perimeter += 1
-                if grid[row][col - 1] == 0 and col > 0:
+                if col > 0 and grid[row][col - 1] == 0:
                     perimeter += 1
                 if col == 0:
                     perimeter += 1
-                if grid[row][col + 1] == 0 and col < cols - 1:
+                if col < cols - 1 and grid[row][col + 1] == 0:
                     perimeter += 1
     return perimeter
