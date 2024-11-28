470168d876632d2942333731bc8a6920184eef0b
Spacefox95 <nathan.raynal@gmail.com>
2024-11-28 09:51:57 +0100

    ok

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -2,24 +2,26 @@
 
 """ Algorithm to determine if the perimeter if island"""
 
+
 def island_perimeter(grid: list[list[int]]) -> int:
+    """ Main funciton """
 
-	perimeter = 0
-	rows, cols = len(grid), len(grid[0])
+    perimeter = 0
+    rows, cols = len(grid), len(grid[0])
 
-	for row in range(rows):
-		for col in range(cols):
-			if grid[row][col] == 1:
-				if grid[row - 1][col] == 0 and row > 0:
-					perimeter += 1
-				if row == 0:
-					perimeter += 1
-				if grid[row +1][col] == 0 and row < rows - 1:
-					perimeter += 1
-				if grid[row][col - 1] == 0 and col > 0:
-					perimeter += 1
-				if col == 0:
-					perimeter += 1
-				if grid[row][col + 1] == 0 and col < cols -1:
-					perimeter += 1
-	return perimeter
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                if grid[row - 1][col] == 0 and row > 0:
+                    perimeter += 1
+                if row == 0:
+                    perimeter += 1
+                if grid[row + 1][col] == 0 and row < rows - 1:
+                    perimeter += 1
+                if grid[row][col - 1] == 0 and col > 0:
+                    perimeter += 1
+                if col == 0:
+                    perimeter += 1
+                if grid[row][col + 1] == 0 and col < cols - 1:
+                    perimeter += 1
+    return perimeter
