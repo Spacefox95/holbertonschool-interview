9f87d59f527c65e60490b243baab806bb2cdb25a
Spacefox95 <nathan.raynal@gmail.com>
2025-03-27 14:22:20 +0100

    ok

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -4,6 +4,18 @@ Determine the fewest number of coins needed to meet a given amount
 """
 
 
+def _decount_loop(coins, total):
+    count = 0
+    remaining_total = total
+    for coin in coins:
+        coin_number = remaining_total // coin
+        count += coin_number
+        remaining_total -= coin_number * coin
+        if remaining_total == 0:
+            return count
+    return -1
+
+
 def makeChange(coins, total):
     """ Calculate the coins addition to reach total"""
     if total <= 0:
@@ -11,23 +23,12 @@ def makeChange(coins, total):
 
     coins = sorted(coins, reverse=True)
 
-    def decount_loop(coins, total):
-        count = 0
-        remaining_total = total
-        for coin in coins:
-            coin_number = remaining_total // coin
-            count += coin_number
-            remaining_total -= coin_number * coin
-            if remaining_total == 0:
-                return count
-        return -1
-
-    initial_count = decount_loop(coins, total)
+    initial_count = _decount_loop(coins, total)
 
     if initial_count != -1:
 
         if len(coins) > 1:
-            second_count = decount_loop(coins[1:], total)
+            second_count = _decount_loop(coins[1:], total)
             if second_count != -1 and second_count < initial_count:
                 return second_count
 
