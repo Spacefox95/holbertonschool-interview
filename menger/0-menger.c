e61bc90d477a2b455f29ddc044ab6c3f8c0fa018
Spacefox95 <nathan.raynal@gmail.com>
2025-10-24 09:53:14 +0200

    ok

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -6,28 +6,36 @@
  * @level: number of side square
  */
 
-void menger(int level) {
-    if (level < 0)
-        return;
+void menger(int level)
+{
+	if (level < 0)
+		return;
 
-    int size = 1;
-    for (int i = 0; i < level; i++) {
-        size *= 3;
-    }
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            int x = i, y = j;
-            int print = 1;
-            while (x > 0 || y > 0) {
-                if (x % 3 == 1 && y % 3 == 1) {
-                    print = 0;
-                    break;
-                }
-                x /= 3;
-                y /= 3;
-            }
-            putchar(print ? '#' : ' ');
-        }
-        putchar('\n');
-    }
+	int size = 1;
+
+	for (int i = 0; i < level; i++)
+	{
+		size *= 3;
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			int x = i, y = j;
+			int print = 1;
+			while (x > 0 || y > 0)
+			{
+				if (x % 3 == 1 && y % 3 == 1)
+				{
+					print = 0;
+					break;
+				}
+				x /= 3;
+				y /= 3;
+			}
+			putchar(print ? '#' : ' ');
+		}
+		putchar('\n');
+	}
 }
